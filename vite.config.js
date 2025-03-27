4123e2e849175450368d48d5155f5993071e5d20
Mackenzie Snyder <masnyder2@hotmail.com>
2025-03-27 11:02:06 -0400

    fix base again

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/jclauneurolab.github.io/",
+  base: "/jclauneurolab.github.io/jclaulab.github.io/",
   plugins: [react()],
 })

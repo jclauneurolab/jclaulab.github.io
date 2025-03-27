3b35da708b8ecf52f176db2a768764518ea97d06
Mackenzie Snyder <masnyder2@hotmail.com>
2025-03-27 10:22:42 -0400

    fix repo link

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/jclaulab.github.io/",
+  base: "/jclauneurolab.github.io/",
   plugins: [react()],
 })
